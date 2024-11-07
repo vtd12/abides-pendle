c723889e093e79a000babdab3ccc9a1fecdcad58
labrinyang <zhiweiy9125@gmail.com>
2024-11-07 18:23:22 +0800

    update pendle_test.sh

--- pende_test.sh
+++ pende_test.sh
@@ -1,3 +1,5 @@
 cd abides-markets\tests\pendle    
 pytest -o log_cli=true --log-level=DEBUG --log-file=./logs/test_logs.log     
-cd ..\..\..\
+cd ..
+pytest -o log_cli=true --log-level=DEBUG --log-file=./logs/test_logs.log     
+cd ..
